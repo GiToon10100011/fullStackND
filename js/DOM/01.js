00048a30343c8c807824f9e0e37249dc075edbc8
GiToon10100011 <Boon10034@gmail.com>
2025-05-02 10:54:05 +0900

    250502

---
+++ js/DOM/01.js
@@ -0,0 +1,9 @@
+let title = document.createElement("h1");
+let text = document.createTextNode("Dom 객체");
+let combined = title.appendChild(text);
+
+document.body.appendChild(combined);
+
+let img = document.createElement("img");
+
+
